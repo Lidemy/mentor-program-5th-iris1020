24b5736d1ee186603a3ec5a2939174e742c307f0
iris1020 <iris@1020.org>
2021-04-25 00:45:36 +0800

    week2 hw

--- homeworks/week2/hw3.js
+++ homeworks/week2/hw3.js
@@ -1,5 +1,12 @@
 function reverse(str) {
-  
+  var newstr = []
+  var a = str.length
+  for (var i = str.length-1; i >= 0; i--){
+  	newstr[i] = str[i + a - 2 * (i)]
+  }
+  newstr.push(str[0])
+  newstr = newstr.join('')
+  console.log(newstr)
 }
 
 reverse('hello');
--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,8 @@
-function printFactor(n) {
-  
+function printFactor(n){
+	for(var i=0; i<=n; i++){
+		if(n % i === 0){
+			console.log(i)
+		}
+	}
 }
-
 printFactor(10);
