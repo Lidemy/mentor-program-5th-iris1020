a012e3b63abfd06e0d3619377c2bcefc2099a8a8
iris1020 <iris@1020.org>
2021-05-16 01:43:09 +0800

    week4

--- homeworks/week4/hw2.js
+++ homeworks/week4/hw2.js
@@ -1,84 +1,62 @@
 const request = require('request');
-const action = process.argv[2]
+const args = process.argv
 const API_ENDPOINT = 'https://lidemy-book-store.herokuapp.com';
 
-if (action === 'list') {
-	listBooks()
-} else if (action === 'read') {
-	readBook(process.argv[3])		
-} else if (action === 'delete') {
-	deleteBook(process.argv[3])
-} else if (action === 'create') {
-	createBook(process.argv[3])
-} else if (action === 'update') {
-	updateBook(process.argv[3], process.argv[4])
-} else {
-	console.log('unknown action')
+const action = args[2];
+const params = args[3]
+
+switch(action) {
+	case 'list':
+		listBooks();
+		break;
+	case 'read':
+		readBook(params);
+		break;
+	case 'delete':
+		deleteBook(params);
+		break;
+	case 'create':
+		createBook(params);
+		break;
+	case 'update':
+		updateBook(params, args[4]);
+		break;
+	default:
+		console.log('Available commands: list, read, delete, create and update');
 }
 
-function listBooks(){
-	request('https://lidemy-book-store.herokuapp.com/books?_limit=20', function (err, response, body) {
-	  if (err) {
-	  	console.log('err', err)
-	  	return
-	  }
-
-	  let books
-	  try {
-	  	books = JSON.parse(body)
-	  } catch(err) {
-	  	console.log(err)
-	  	return
-	  }
-	  
-	  for (let i = 0; i < books.length; i++) {
-	  console.log(`${books[i].id} ${books[i].name}`)
-	  }
-	});
-
+function listBooks() {
+	request(`${API_ENDPOINT}/books?_limit=20`, (err, res, body) => {
+		if (err) {
+			return console.log('抓取失敗', err);
+		}
+		const data = JSON.parse(body);
+		for (let i = 0; i < data.length; i += 1) {
+			console.log(`${data[i].id} ${data[i].name}`);
+		}
+	})
 }
 
-function readBook(id){
-	request('https://lidemy-book-store.herokuapp.com/books/' + id, function (err, response, body) {
-	  if (err) {
-	  	console.log('err', err)
-	  	return
-	  }
-
-	  let book
-	  try {
-	  	book = JSON.parse(body)
-	  } catch(err) {
-	  	console.log(err)
-	  	return
-	  }
-	  
-	  console.log(book)
-	  });
-	}
-
-function deleteBook(id){
-	request.delete('https://lidemy-book-store.herokuapp.com/books/' + id, function (err, response, body) {
-	  if (err) {
-	  	console.log('err', err)
-	  	return
-	  }
-
-	  let book
-	  try {
-	  	book = JSON.parse(body)
-	  } catch(err) {
-	  	console.log(err)
-	  	return
-	  }
-	  
-	  console.log(book)
-	  });
-
+function readBook(id) {
+	request(`${API_ENDPOINT}/books/${id}`, (err, res, body) => {
+		if (err) {
+			return console.log('抓取失敗', err);
+		}
+		const data = JSON.parse(body);
+		console.log(data);
+	})
 }
 
+function deleteBook(id) {
+	request.delete(`${API_ENDPOINT}/books/${id}`, (err, res, body) => {
+		if (err) {
+			return console.log('刪除失敗', err);
+		}
+		console.log('刪除成功');
+	})
+}
 
-function createBook(name){
+function createBook(name) {
 	request.post({
 		url: `${API_ENDPOINT}/books`,
 		form: {
@@ -90,13 +68,12 @@ function createBook(name){
 		}
 		console.log('新增成功!');
 	})
-
 }
 
-function updateBook(id, newName){
+function updateBook(id, name) {
 	request.patch({
 		url: `${API_ENDPOINT}/books/${id}`,
-		form:{
+		form: {
 			name
 		}
 	}, (err, res) => {
